966b9b1fa1693143eec27e1ff99f2ad05db6d12b
unknown <akilapanunayan@gmail.com>
2024-06-23 17:08:56 +0100

    init

--- app/page.js
+++ app/page.js
@@ -6,27 +6,27 @@ import linesBg from '/public/imgs/lines-bg.png';
 
 export default function Home() {
 	return (
-		<main className="h-screen w-full bg-[--bg] flex flex-col relative">
-			<div className="flex-1 container">
-				<div className="h-full w-full flex flex-col items-center justify-center text-center gap-[35px] xl:gap-[50px] max-w-[800px] mx-auto">
+		<main className="min-h-screen w-full bg-[--bg] flex flex-col relative">
+			<div className="flex-1 container py-[35px] flex flex-col items-center justify-center relative z-[10]">
+				<div className="h-full w-full text-center space-y-[35px] xl:space-y-[35px] max-w-[800px] mx-auto">
 					<div className="">
 						<div className="">
 							<Image
 								src={logo}
 								alt="logo"
-								className="object-cover h-[45px] md:h-[60px] w-full"
+								className="object-contain h-[50px] md:h-[70px] w-full"
 							/>
 						</div>
-						<p className="mt-[-5px] uppercase text-[--highlight] tracking-widest">
+						<p className="mt-[-5px] md:mt-[-10px] uppercase text-[--highlight] tracking-widest text-xs md:text-base">
 							Scotch Whisky since 1937
 						</p>
 					</div>
-					<div className="border border-[--highlight] p-5 md:p-7 space-y-3 md:space-y-7">
+					<div className="border border-[--highlight] p-5 py-6 md:p-7 space-y-6 md:space-y-7">
 						<h3 className="uppercase text-[--highlight]">Age Verification</h3>
 						<h3 className="uppercase text-[--white]">
 							You must be over 21 to access this website
 						</h3>
-						<div className="flex items-center justify-center gap-3">
+						<div className="flex flex-col md:flex-row items-center justify-center gap-3">
 							<Link href="/home" className="btn-1">
 								I am over 21
 							</Link>
@@ -41,20 +41,20 @@ export default function Home() {
 					</div>
 				</div>
 			</div>
-			<div className="w-full">
+			<div className="w-full relative">
 				<Image
 					src={pattern}
 					alt="pattern"
-					className="h-[100px] w-full object-cover"
+					className="h-[10vh] md:h-[15vh] max-h-[100px] w-full object-cover"
 				/>
 			</div>
 
 			{/* LINES */}
-			<div className="fixed top-0 left-[60%] h-full pb-[80px]">
+			<div className="absolute bottom-[10vh] md:top-0 left-[50%] lg:left-[60%] h-full pb-[12vh]">
 				<Image
 					src={linesBg}
 					alt="logo"
-					className="object-contain h-full w-auto"
+					className="object-cover md:object-contain object-left-bottom h-full w-auto"
 				/>
 			</div>
 		</main>
